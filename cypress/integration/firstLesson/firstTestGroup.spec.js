308948233784cd04e9250430c97ba03038cc8257
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-05-08 17:37:59 +0200

    First tests

---
+++ cypress/integration/firstLesson/firstTestGroup.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+context('Entering selected pages tests', () => {
+
+  // describe('Google test', () => {
+  //   it('test 1', () => {
+  //     cy.visit('www.google.com');
+  //     cy.url().should('contain', 'https://www.google');
+  //     cy.get('#L2AGLb > .QS5gu').click()
+  //     cy.get('.dbsFrd').should('not.be.visible');
+  //   });
+  // })
+  
+  describe('WP test', () => {
+    it('test 2', () => {
+      cy.visit('www.wp.pl');
+      cy.url().should('contain', 'wp.pl'); 
+      cy.get('body > div > div > div > div > div > button:nth-child(2');
+    });
+  })
+})
+
+
+
