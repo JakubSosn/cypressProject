0d114f6d3ec12fff4ab86ee9c217aada25a06d2a
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-06-19 21:23:34 +0200

    cleaning for newProject

--- cypress/integration/secondLesson/wpTest.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types='cypress'/>
-
-describe('WP Test', ()=> {
-  it('Test 1', ()=> {
-    cy.visit('www.wp.pl');
-    cy.url().should('contain', 'wp.pl'); 
-    cy.get('body > div > div > div > div > div > button:nth-child(2').click();
-  })
-})
