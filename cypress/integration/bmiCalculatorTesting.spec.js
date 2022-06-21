ca76b9dd60aecee8067c49a91f84107812a30e74
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-06-21 21:00:51 +0200

    add test and tags

--- cypress/integration/bmiCalculatorTesting.spec.js
+++ cypress/integration/bmiCalculatorTesting.spec.js
@@ -7,36 +7,35 @@ context('Entering selected my bmiCalculator for tests its functionality', () => 
     beforeEach(() => {
       cy.visit('https://jakubsosn.github.io/Projekt-JS-PB/');
       cy.url().should('contain', 'https://jakubsosn.github.io/Projekt-JS-PB/');
-      // cy.viewport(1240, 760);
       cy.fixture('bmiCalcData.json').as('data');
     })
 
-    it('Checking is the weight field is empty', () => {
+    it('Checking is the weight field is empty',{tags:'empty'}, () => {
       cy.get('.weight').should('be.empty');
     })
 
-    it('Checking is the height field is empty', () => {
+    it('Checking is the height field is empty',{tags:'empty'}, () => {
       cy.get('.height').should('be.empty');
     })
 
-    it('Checking is history of entries is empty', () => {
+    it('Checking is history of entries is empty',{tags:'empty'}, () => {
       cy.get('.right p').should('be.empty');
       cy.get('.right .button').should('not.be.visible');
     })
 
-    it('Checking correct font-weight of header', () => {
+    it('Checking correct font-weight of header',{tags:'correct'}, () => {
       cy.get('header').should('have.css', 'font-weight', '700');
     })
 
-    it('Checking correct input weight background color', () => {
+    it('Checking correct input weight background color',{tags:'correct'}, () => {
       cy.get('.weight').should('have.css', 'background-color', 'rgb(239, 239, 239)');
     })
 
-    it('Checking correct background color of "Zatwierdź" button', () => {
+    it('Checking correct background color of "Zatwierdź" button',{tags:'correct'}, () => {
       cy.get('form > .button').should('have.css', 'background-color', 'rgb(54, 176, 60)');
     })
 
-    it('Filling weight and height field and comparison the result data', function () {
+    it('Filling weight and height field and comparison the result data',{tags:'result'}, function () {
       cy.get('.weight').clear().type(this.data[0].weight);
       cy.get('.height').clear().type(this.data[0].height);
       cy.get('form > .button').click();
@@ -44,7 +43,7 @@ context('Entering selected my bmiCalculator for tests its functionality', () => 
       cy.get('.middle > :nth-child(3)').should('have.text', this.data[0].weightResult);
     })
 
-    it('Filling weight and height field and checking BMI result', function () {
+    it('Filling weight and height field and checking BMI result',{tags:'result'}, function () {
       cy.get('.weight').clear().type(this.data[1].weight);
       cy.get('.height').clear().type(this.data[1].height);
       cy.get('form > .button').click();
